a9c9c307211a39ed3d7f985534c19fb93e0e1a4e
jonatanriq <jonatan@Rede-de-Jonatan.local>
2022-04-14 12:36:13 -0300

    coloquei o (dado)

--- teste_normalidade.py
+++ teste_normalidade.py
@@ -1,7 +1,7 @@
-def teste_normalidade():
+def teste_normalidade(dado):
 	from scipy.stats import normaltest
 	alpha = 0.05
-	k2, p = normaltest(boia['Hsig'][boia.index < '2020-01'])
+	k2, p = normaltest(dado)
 
 	# se o valor p for maior do que alpha significa que a hipotese nula não pode ser rejeitada,
 	# ou seja, a distribuição é normal
